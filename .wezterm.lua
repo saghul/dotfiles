5dc1a6847bb140e5ccb53a5fe4fbd6067d83f10c
Saúl Ibarra Corretgé <s@saghul.net>
2025-04-09 22:05:08 +0200

    wzterm: add config

---
+++ .wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'SauceCodePro Nerd Font Mono'
+config.font_size = 16.0
+
+config.color_scheme = 'Dracula (Official)'
+
+config.window_decorations = "RESIZE"
+
+return config
+
